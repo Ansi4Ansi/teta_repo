a9d08324efe9068efaef90687af183679fb5d647
Andrey Simkin <andrei.simkin@gmail.com>
2022-03-01 00:47:28 +0300

    add kv_strorage template

--- teta_kvstorage/main.go
+++ teta_kvstorage/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
-	fmt.Println("Main package")
+import (
+	"context"
+	"sync"
+)
+
+type SafeContext struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+type KVStorage interface {
+	Get(ctx context.Context, key string) (interface{}, error)
+	Put(ctx context.Context, key string, val interface{}) error
+	Delete(ctx context.Context, key string) error
+}
+
+func (c *SafeContext) Get(ctx context.Context, key string) (value interface{}, err error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if ctx != nil {
+		value = ctx.Value(key)
+	}
+	return
+}
+func (c *SafeContext) Put(ctx context.Context, key string, val interface{}) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if ctx != nil {
+		ctx.WithValue(ctx, key, val)
+	}
+}
+
+func (c *SafeContext) Delete(ctx context.Context, key string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 }
