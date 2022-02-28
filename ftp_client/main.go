0effce4257647d9cb44444e5ba3f1e03c5f9be8a
Andrey Simkin <andrei.simkin@gmail.com>
2022-02-28 23:34:42 +0300

    add main.go

---
+++ ftp_client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/Ansi4Ansi/ftp_client/ftp"
+)
+
+func main() {
+	conn, err := ftp.Connect("localhost", 21)
+	if err != nil {
+		fmt.Println("unable to connect, error:", err)
+		return
+	}
+	defer conn.Close()
+
+	err = conn.Login("user", "password")
+	if err != nil {
+		fmt.Println("login failed, error:", err)
+		return
+	}
+	defer conn.Quit()
+
+	source := bytes.NewBuffer([]byte("This is the file content."))
+	err = conn.Upload(source, "/example_upload.txt")
+	if err != nil {
+		fmt.Println("unable to upload file, error:", err)
+	}
+}
